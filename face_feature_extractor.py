51f8af887a32d50d4472a68c25aa723a07b239de
Julian <julian.thier@gmx.de>
2024-02-24 20:53:11 +0100

    cleaned all my code files. Removed irrelevant code files

--- face_feature_extractor.py
+++
@@ -1,6 +0,0 @@
-def face_feature_extractor():
-    """
-    conducts face model classifications and saves feature maps
-    :return: saves classifications for pictures with face model; saves feature maps for faces in all necessary stages
-    """
-    print("This has not be implemented yet.")
--- motion_feature_extractor.py
+++
@@ -1,6 +0,0 @@
-def motion_feature_extractor():
-    """
-    reads in unprocessed videos and creates motion flow representation for each video
-    :return: saves the motion representation
-    """
-    print("This has not be implemented yet.")
